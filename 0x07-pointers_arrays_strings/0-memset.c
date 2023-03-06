8763851427d55933943c4ca13dea539307dc373f
hertheyhermee <tiamiyuhayzed@gmail.com>
2023-03-06 06:29:04 -0800

    low level

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - ironnou
+ * @s: pointer to char params
+ * @b: data to change
+ * @n: index
+ * Return: *s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int j;
+
+	for (j = 0; j < n; j++)
+	{
+		s[j] = b;
+	}
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,5 @@
+<h1>MORE POINTERS, STRINGS AND ARRAYS</h1>
+<p>This directory contains task files that show practices on arrays, strings and pointers. SOmeof the tasks include:</p>
+<ul>
+	<li>0-memset.c</li>
+</ul>
