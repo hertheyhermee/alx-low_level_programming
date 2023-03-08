7c662b1ca4bf2b9090b225db55306ccc0f18bdfe
hertheyhermee <tiamiyuhayzed@gmail.com>
2023-03-08 04:55:55 -0800

    putchar

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -15,16 +15,3 @@ void _puts_recursion(char *s)
 	else
 		_putchar('\n');
 }
-
-#include <stdio.h>
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    _puts_recursion("Puts with recursion");
-    return (0);
-}
