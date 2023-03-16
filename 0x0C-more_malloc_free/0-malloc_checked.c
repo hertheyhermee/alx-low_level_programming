f03bcdcaed60d1791cf6276c0cc289e5fa77f804
hertheyhermee <tiamiyuhayzed@gmail.com>
2023-03-16 04:06:43 -0700

    malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *malloc_checked - a function that returns a pointer to a newly allocated space in memory.
+ * Returns NULL if str = NULL
+ * On success, the _strdup function returns a pointer to the duplicated string.
+ * Returns NULL if insufficient memory was available
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+	
+	if (ptr == NULL)
+	{
+		exit (98);
+	}
+	return (ptr);
+}
