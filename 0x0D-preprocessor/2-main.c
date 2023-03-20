93a78455457b2e3ab4237c1c8299984f4171b9fb
hertheyhermee <tiamiyuhayzed@gmail.com>
2023-03-20 04:47:43 -0700

    main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point - a program that prints the name of file it was compiled from
+ * Return: (0) success
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
