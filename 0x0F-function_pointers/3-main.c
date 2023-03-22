245572d3ab1666dcff0a06d02375effd5087c4a8
hertheyhermee <tiamiyuhayzed@gmail.com>
2023-03-22 13:48:51 -0700

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,36 +1,47 @@
 #include "3-calc.h"
 
 /**
- * main - Entry point of program
+ * main - program that perfroms simple operations
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
- * @argc: arguement count
- * @argv: arguement vector
- *
- * Return: 0 (success)
- *
+ * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
+	int arg1, arg2, result;
+	char o;
 	int (*func)(int, int);
-	int num1, num2;
 
 	if (argc != 4)
 	{
-		printf("Error\n"), exit(98);
+		printf("Error\n");
+		exit(98);
 	}
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
+	arg1 = atoi(argv[1]);
+	arg2 = atoi(argv[3]);
+
 	func = get_op_func(argv[2]);
 
 	if (!func)
 	{
-		printf("Error\n"), exit(99);
+		printf("Error\n");
+		exit(99);
 	}
-	if ((argv[2][0] == '/' || argv[2][0] == '%') && atoi(argv[3]) == 0)
+
+	o = *argv[2];
+
+	if ((o == '/' || o == '%') && arg2 == 0)
 	{
-		printf("Error\n"), exit(100);
+		printf("Error\n");
+		exit(100);
 	}
-	printf("%d\n", func(num1, num2));
+
+	result = func(arg1, arg2);
+
+	printf("%d\n", result);
+
 	return (0);
 }
+
