bb5ad877c5592d5eb8f02816a2958740d6bcc5e6
hertheyhermee <tiamiyuhayzed@gmail.com>
2023-04-03 08:44:07 -0700

    print number of nodes

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * print_listint - a function that prints all elements of a listint_t list
+ * @h: pointer to the head mode
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	for (; h != NULL; h = h->next)
+	{
+		printf("%d\n", h->n);
+		count++;
+	}
+	return (count);
+}
