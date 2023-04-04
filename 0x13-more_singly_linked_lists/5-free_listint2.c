f0c1eeaba2312c7f50875bd5ce8f3f464344134c
hertheyhermee <tiamiyuhayzed@gmail.com>
2023-04-04 07:09:28 -0700

    It is worth the wait

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,10 +7,12 @@
  */
 void free_listint2(listint_t **head)
 {
+	listint_t *current;
+
 	if (head == NULL || *head == NULL)
 		return;
 
-	listint_t *current = *head;
+	current = *head;
 
 	while (current != NULL)
 	{
--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,19 +1,20 @@
 #include "lists.h"
 
 /**
- * pop_listint - a funtion tht deletes the head node of a linked list and
- * returns the head node's data(n)
- * @n: head node data
+ * pop_listint - a funtion tht deletes the head node of a linked list
  * @head: pointer
  * Return: zero(0) if linked list is empty
  */
 int pop_listint(listint_t **head)
 {
+	listint_t *temp;
+	int n;
+
 	if (head == NULL || *head == NULL)
 		return (0);
 
-	listint_t *temp = *head;
-	int n = temp->n;
+	temp = *head;
+	n = temp->n;
 	*head = (*head)->next;
 	free(temp);
 	return (0);
