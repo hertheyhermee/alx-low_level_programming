e2ce1e9ee0a5425f709ee3c3fe92f958d081dad2
hertheyhermee <tiamiyuhayzed@gmail.com>
2023-04-09 10:35:19 -0700

    text file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,32 +15,21 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int fd;
 	char *buffer;
 
-	if (filename == NULL)
+	if (filename == NULL || letters == 0)
 		return (0);
 	fd = open(filename, O_RDDONLY);
 	if (fd == -1)
 		return (0);
-	buffer = malloc(letters + 1);
+	buffer = malloc(sizeof(cha)*(letters));
 	if (buffer == NULL)
-	{
-		close(fd);
 		return (0);
-	}
 	read_len = read(fd, buffer, letters);
 	if (read_len == -1)
-	{
-		free(buffer);
-		close(fd);
 		return (0);
-	}
-	buffer[read_len] = '\0';
-	write_len = write(STDOUT_FILENO, buffer, read_len)
-	{
-		free(buffer);
-		close(fd);
-		return (0);
-	}
+	write_len = write(STDOUT_FILENO, buffer, read_len);
 	free(buffer);
 	close(fd);
-	return (read_len);
+	if (read_len != write_len)
+		return (0);
+	return (write_len);
 }
