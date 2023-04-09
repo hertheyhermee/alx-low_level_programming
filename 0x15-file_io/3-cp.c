9b23bf4726965fbf567cc8c587defaf57e86317a
hertheyhermee <tiamiyuhayzed@gmail.com>
2023-04-09 15:50:44 -0700

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,27 +8,27 @@
  */
 int main(int argc, char *argv[])
 {
-	int going_to;
-	int coming_from;
+	int file_to;
+	int file_from;
 	ssize_t read_length;
 	ssize_t write_length;
 	char buffer[1024];
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp coming_from going_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	coming_from = open(argv[1], O_RDONLY);
-	if (coming_from == -1)
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	going_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 00664);
-	while ((read_length = read(coming_from, buffer, 1024)) != 0)
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 00664);
+	while ((read_length = read(file_from, buffer, 1024)) != 0)
 	{
-		if (going_to == -1)
+		if (file_to == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
@@ -38,21 +38,21 @@ int main(int argc, char *argv[])
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
 		}
-		write_length = write(going_to, buffer, read_length);
+		write_length = write(file_to, buffer, read_length);
 		if (write_length != read_length || write_len == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
 	}
-	if (close(coming_from) == -1)
+	if (close(file_from) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", coming_from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
-	if (close(going_to) == -1)
+	if (close(file_to) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", going_to);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
 		exit(100);
 	}
 	return (0);
