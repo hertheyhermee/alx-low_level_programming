3db6dffc4f5664c13601b41784129589367d0100
hertheyhermee <tiamiyuhayzed@gmail.com>
2023-04-09 15:16:25 -0700

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,7 +3,7 @@
 /**
  * create_file - creates a file
  * @filename: name of the file to create
- * @text_content: NULL terminated string 
+ * @text_content: NULL terminated string
  * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
