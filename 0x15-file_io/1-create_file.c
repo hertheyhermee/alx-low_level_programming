f773441cad52f38e0bbb74a3e9b2bcffa238d033
hertheyhermee <tiamiyuhayzed@gmail.com>
2023-04-09 14:38:40 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,36 +3,40 @@
 /**
  * create_file - a function that creates a file
  * @filename: name of file to create
- * @text_context: NULL terminated string
+ * @text_content: NULL terminated string
  * Return: 1 on success, -1 on failure
  * -1, if filename is NULL
  */
-int _string_len(const char *s)
-{
-	return (strlen(s));
-}
-
 int create_file(const char *filename, char *text_content)
 {
-	int fd, len;
-	ssize_t n;
+	int fd, len = 0, n = 0;
 
 	if (filename == NULL)
 		return (-1);
-
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if (fd == -1)
+	if (filename == NULL)
 		return (-1);
 	if (text_content != NULL)
 	{
 		len = _string_len(text_content);
 		n = write(fd, text_content, len);
-		if (n == -1)
-		{
-			close(fd);
-			return (-1);
-		}
 	}
+	if (n == -1)
+		return (-1);
 	close(fd);
 	return (1);
+}
+
+/**
+ * _string_len - a function that gets the length of a string
+ * @s: string
+ * Return: string length
+ */
+
+int _string_len(const char *s)
+{
+	int len;
+
+	while (*(s + len))
+		++len;
+	return (len);
 }
