ec56ac7517034767a1c92ea129822f73a141138a
hertheyhermee <tiamiyuhayzed@gmail.com>
2023-04-10 14:24:46 -0700

    renewal

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,30 +1,50 @@
 #include "main.h"
-#include "funtions.h"
+#define BUFSIZE 1024
 
 /**
- * main - Entry point
- * @argc: Number of command-line arguments
- * @argv: Array containing the command-line arguments
- *
- * Return: Always 0
+ * main - This program copies the content of a file to another file
+ * @argc: Argument count parameter
+ * @argv: argument vector parameter
+ * Return: 0 when successful otherwise, an exit status
  */
 int main(int argc, char *argv[])
 {
-	int file_to, file_from;
+	int file_from;
+	int file_to;
+	int close_in, close_out;
+	ssize_t reader, writer;
+	char buffer[BUFSIZE];
 
 	if (argc != 3)
-	{
-		print_usage_error();
-		exit(97);
-	}
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 
-	file_from = open_file_for_reading(argv[1]);
-	file_to = open_file_for_writing(argv[2]);
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
+			exit(98);
 
-	copy_file_contents(file_from, file_to);
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (file_to == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 
-	close_file(file_from);
-	close_file(file_to);
+	do {
+		reader = read(file_from, buffer, BUFSIZE);
+		if (reader == -1)
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
+				exit(98);
+
+		writer = write(file_to, buffer, reader);
+		if (writer == -1)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	} while (reader > 0);
+
+	close_in = close(file_from);
+	if (close_in == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
+
+	close_out = close(file_to);
+	if (close_out == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
 
 	return (0);
 }
