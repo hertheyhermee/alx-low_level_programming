d18592c6c38747d0b3179fe118c30f4039f9326d
hertheyhermee <tiamiyuhayzed@gmail.com>
2023-04-10 14:04:37 -0700

    renewal

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,59 +1,29 @@
 #include "main.h"
 
 /**
- * main - copies the content of a file to another file
- * @argc: arguement c
- * @argv: arguement v
- * Return: 0 on success
+ * main - Entry point
+ * @argc: Number of command-line arguments
+ * @argv: Array containing the command-line arguments
+ *
+ * Return: Always 0
  */
 int main(int argc, char *argv[])
 {
-	int file_to;
-	int file_from;
-	ssize_t read_length;
-	ssize_t write_length;
-	char buffer[1024];
+	int file_to, file_from;
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		print_usage_error();
 		exit(97);
 	}
-	file_from = open(argv[1], O_RDONLY);
-	if (file_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 00664);
-	while ((read_length = read(file_from, buffer, 1024)) != 0)
-	{
-		if (file_to == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
-		if (read_length == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			exit(98);
-		}
-		write_length = write(file_to, buffer, read_length);
-		if (write_length != read_length || write_length == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
-	}
-	if (close(file_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-		exit(100);
-	}
-	if (close(file_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
-		exit(100);
-	}
+
+	file_from = open_file_for_reading(argv[1]);
+	file_to = open_file_for_writing(argv[2]);
+
+	copy_file_contents(file_from, file_to);
+
+	close_file(file_from);
+	close_file(file_to);
+
 	return (0);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -13,5 +13,10 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _string_len(char *s);
+void error_handler(int err_code, char *file_name, int fd_from, int fd_to);
+void copy_file_content(int fd_from, int fd_to, char *file_from, char *file_to);
+int open_file(char *file_name, int flag, mode_t mode);
+void close_file(int fd);
+
 
 #endif
